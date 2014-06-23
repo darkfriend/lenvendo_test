8448755edfb758af821ed7c0472e2213d3628fa1
maslov <maslov.tlt@gmail.com>
2014-06-23 14:29:51 +0400

    Поправил косяк с именем файла

--- application/models/pictures_model.php
+++ application/models/pictures_model.php
@@ -6,6 +6,7 @@ class pictures_model extends model{
     }
     public function set_data($data) {
         if(!$data['user_id'] || !$data['picture_name']) return false;
+        $data['picture_name'] = $data['picture_name'][0]=='/' ? substr($data['picture_name'], 1) : $data['picture_name'];
         $result = $this->db->query("INSERT INTO user_pictures (id_user,name_file,last_edit,create_date) VALUES ( ?i, ?s, ?s, ?s )", $data['user_id'], $data['picture_name'], date('d.m.Y H:i:s'), date('m.Y'));
         return $result;
     }
