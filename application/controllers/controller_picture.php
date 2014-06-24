9ea833e32b5ccaaef780f2fc1703ac39bd1d990a
maslov <maslov.tlt@gmail.com>
2014-06-24 13:42:21 +0400

    добавил метод mergePictures и сопутствующий метод возврата info картинки

--- application/controllers/controller_picture.php
+++ application/controllers/controller_picture.php
@@ -5,6 +5,13 @@ class controller_picture extends controller {
             $hashFile,
             $msg;
     
+    private $flag = array( //поддерживаемые расширения
+                1=>'GIF',
+                2=>'JPG',
+                3=>'PNG',
+            );
+
+
     public function action_index(){
         
     }
@@ -78,11 +85,12 @@ class controller_picture extends controller {
                 $this->msg = 'Хэш не найден';
             } elseif( $format = $this->getRequestQuery('format') ) {
                 
-                $name = md5($this->getRequestQuery('data').time());
-                $file = "/$name.".$format;
+                //$name = md5($this->getRequestQuery('data').time());
+                //$file = "/$name.".$format;
                 //var_dump($format);
+                $pathFile = $this->mergePictures($pict1, $pict2, USER_IMAGE_EDIT_MARGE);
                 if (!file_exists($file)){
-                    $pathToFile = $this->saveRequestFile( array($file , $this->getRequestQuery('data')) );
+                    //$pathToFile = $this->saveRequestFile( array($file , $this->getRequestQuery('data')) );
                     if (file_exists($pathToFile)){
                         
                         if($resultInsert = $this->getModel()->set_data( array( 'picture_name'=>$file, 'user_id'=>$this->getUser() ) )){
@@ -182,19 +190,88 @@ class controller_picture extends controller {
         }*/
     }
     
-    #сохраняет нарисованную картинку
-    protected function saveRequestFile($file,$data){
+    /*
+     * сохраняет нарисованную картинку
+     * @param string $file - имя файла с расширением
+     * @param string $data - base64 картинки
+     * @param string $path_file - абсолютный путь до сохраняемого файла
+     * @return string $path_file - абсолютный путь до сохраняемого файла
+     */
+    protected function saveRequestFile($file,$data,$path_file){
         $image = str_replace(" ", "+", $data);
         $image = substr($image, strpos($image, ","));
         //$dirDateMonth = date('m.Y');
-        $path_file = PATH_ROOT.PATH_TO_SAVE_IMG.date('m.Y').$file;
+        if(!$path_file){
+            $path_file = PATH_ROOT.PATH_TO_SAVE_IMG.date('m.Y').$file;
+        } else {
+            $path_file.=$file;
+        }
         file_put_contents($path_file, base64_decode($image));
         return $path_file;
     }
     
-    private function mergePictures($pict1, $pict2){
-        if($pict1['typeImg']!==$pict2['typeImg']) return false;
-        switch($pict1['typeImg']){
+    /*
+     * совмещает 2 картинки в 1 новую или перезаписывает имеющуюся
+     * @param array $pict1 - 'typeImg'=>'расширение картинки', 'path'=>'абсолютный путь до картинки'
+     * @param array $pict2 - 'typeImg'=>'расширение картинки', 'path'=>'абсолютный путь до картинки'
+     * @param boolean $merge true|false (false default) - совместить с $pict1 или создать новую
+     * @return string $path_file - абсолютный путь до совмещенного файла
+     */
+    private function mergePictures($pict1, $pict2, $merge=false){
+        //if($pict1['typeImg']!==$pict2['typeImg']) return false;
+        $sizePict1 = $this->getSizeTypeImage($pict1['path']);
+        $sizePict2 = $this->getSizeTypeImage($pict2['path']);
+        
+        if($sizePict1['type'] !== $sizePict2['type']) return false;
+        
+        //list($sizePict1['width'], $sizePict1['height']) = getimagesize($pict1['path']);
+        //list($sizePict2['width'], $sizePict2['height']) = getimagesize($pict2['path']);
+        //$sizePict1['type'] = 'png';
+        switch($sizePict1['type']){
+            case 'jpg' :
+                $src1 = imagecreatefromjpeg($pict1['path']);
+                    imagealphablending($src1, true);
+                $src2 = imagecreatefromjpeg($pict2['path']);
+                    imagealphablending($src2, true);
+                break;
+            case 'png' :
+                $src1 = imagecreatefrompng($pict1['path']);
+                    imagealphablending($src1, true);
+                    imagesavealpha($src1, true);
+                $src2 = imagecreatefrompng($pict2['path']);
+                    imagealphablending($src2, true);
+                    imagesavealpha($src2, true);
+                break;
+        }
+        
+        if(!$src1 || !$src2) return false;
+        
+        if($merge){
+            $genFile = $pict1['path'];
+        } else {
+            $genFile = PATH_ROOT.PATH_TO_SAVE_IMG.date('m.Y').md5(time()).'.'.$sizePict1['type'];
+        }
+        
+        if(imagecopy($src1, $src2, 0, 0, 0, 0, $sizePict1['width'], $sizePict1['height'])){
+            
+            switch($sizePict1['type']){
+                case 'jpg' :
+                    imagejpeg($src1, $genFile );
+                    break;
+                case 'png' :
+                    imagepng($src1, $genFile );
+                    break;
+            }
+            
+            imagedestroy($src1);
+            imagedestroy($src2);
+            
+            return $genFile;
+        } else {
+            return false;
+        }
+        
+        /*switch($pict1['typeImg']){
             case 'jpg' :
                 $src1 = imagecreatefromjpeg($pict1['path']);
                 $src2 = imagecreatefromjpeg($pict2['path']);
@@ -209,6 +286,34 @@ class controller_picture extends controller {
             return $pict1['path'];
         } else {
             return false;
+        }*/
+        
+    }
+    
+    /*
+     * возвращает расширение картинки
+     * @param string $path - путь до картинки
+     * @param boolean $absolute - абсолютный ли путь указан в $path
+     * @return string|false - расширение файла
+     */
+    public function getSizeTypeImage($path, $absolute = true, $return=array('width','height','type')) {
+        if(!$absolute){
+            $path = PATH_ROOT.$path;
         }
+        list($img['width'],$img['height'],$img['type'])= getimagesize($path);
+        $result = array();
+        foreach($return as $key=>$val){
+            switch ($val){
+                case 'type' :   
+                    if(!empty($this->flag[$img['type']])){
+                        $result['type'] = $this->flag[$img['type']];
+                    } else {
+                        $result = false;
+                        break;
+                    }
+                default : $result[$val] = $img[$val];
+            }
+        }
+        return $result;
     }
 }
