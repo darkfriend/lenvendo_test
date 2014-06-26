4455ab7fdee9762d7ea6b5595aa1256374efee45
maslov <maslov.tlt@gmail.com>
2014-06-26 16:07:13 +0400

    добавил ссылки

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -3,8 +3,8 @@
     <div class="inner cover block-center well well-lg">
         <p class="lead text-center">Сохранение и редактирование растровой картинки нарисованной в canvas</p>
         <p class="lead btn-group">
-            <a href="#" class="btn btn-primary btn-lg">Создать рисунок</a>
-            <a href="#" class="btn btn-info btn-lg">Читать мануал</a>
+            <a href="/picture/add/" class="btn btn-primary btn-lg">Создать рисунок</a>
+            <a href="/main/readme/" class="btn btn-info btn-lg">Читать мануал</a>
         </p>
     </div>
 </div>
